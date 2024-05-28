c46d7b95748305cb7d025946a1f7d5e3fe0317bb
Osama Adam <osamaadamme@gmail.com>
2024-05-28 14:07:06 +0300

    cfgrr push (Tue, 28 May 2024 14:07:06 EEST)

--- home/.config/nvim/after/plugin/lualine.lua
+++ home/.config/nvim/after/plugin/lualine.lua
@@ -4,13 +4,4 @@ require("lualine").setup({
     icons_enabled = true,
   },
   extensions = { "fugitive", "neo-tree", "toggleterm", "man" },
-  sections = {
-    lualine_x = {
-      {
-        require("noice").api.statusline.mode.get,
-        cond = require("noice").api.statusline.mode.has,
-        color = { fg = "#ff9e64" },
-      },
-    },
-  },
 })
